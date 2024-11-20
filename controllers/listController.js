ec77264de516ab52e8152779b4568fa2a44a86bc
vow130504 <22120344@student.hcmus.edu.vn>
2024-11-20 16:47:34 +0700

    merge searching and filtering

--- controllers/listController.js
+++ controllers/listController.js
@@ -1,10 +1,17 @@
 const bookService = require('../components/books/bookService');
-
 exports.getList = async (req, res) => {
   try {
-    const books = await bookService.getAllBooks();
+    const { genre, author, price, purchaseCount } = req.query;
+    
+    // Thực hiện lọc sách theo các tham số truyền vào
+    const books = await bookService.getAllBooks(genre, author, price, purchaseCount);
     const genres = await bookService.getGenres();
-    res.render('listBook', {books,genres});
+    const authors = await bookService.getAuthors();
+    
+    const prices = await bookService.getPrices();
+
+    
+    res.render('listBook', { books, genres, authors, prices });
   } catch (error) {
     console.error('Error fetching books:', error);
     res.status(500).send('Error fetching books');
