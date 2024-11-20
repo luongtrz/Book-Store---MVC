eb1546960932689e2019af3621a51bd2a2238976
luongtrz <luongtran1832004@gmail.com>
2024-11-20 14:21:47 +0700

    fix session cookie in vercel

--- middlewares/sessionMiddleware.js
+++ middlewares/sessionMiddleware.js
@@ -1,10 +1,15 @@
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const sessionMiddleware = session({
     secret: 'luongtrz',
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } // Set to true if using HTTPS
+    cookie: { secure: false }, // Set to true if using HTTPS
+    store: MongoStore.create({
+        mongoUrl: 'mongodb+srv://luongtrz:luongtrzpass@book.hrsim.mongodb.net/BookStore',
+        collectionName: 'sessions'
+    })
 });
 
 module.exports = sessionMiddleware;
