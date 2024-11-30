6d4c92dd50fe8df9e403ceb70158a08597e26286
luongtrz <luongtran1832004@gmail.com>
2024-11-30 16:54:57 +0700

    add key env google authen for vercel

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -29,7 +29,7 @@ passport.use(new LocalStrategy(
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:5000/auth/google/callback'
+  callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:5000/auth/google/callback'
 }, async (token, tokenSecret, profile, done) => {
   try {
       let user = await userService.findUserByGoogleId(profile.id);
